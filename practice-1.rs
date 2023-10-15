ef08db0f76f2a0063b087a00988495b28e0e7530
gngugnniokl <75842539+gngugnniokl@users.noreply.github.com>
2023-10-15 18:35:50 +0100

    (My first commit)

---
+++ practice-1.rs
@@ -0,0 +1,12 @@
+fn main(){
+	println!("Welcome to COS 101")
+
+	println!("The curse learning outcome is as follows:")
+
+	prinln!("1. Distnguish between computer and computer programming,/n 
+		     2. Develop some teqniques in computer science,/n
+		     3. Understand the different areas of study in computer science,/n 
+		     4. Conversant with applications of computer science./n 
+		     5.Navigate the career prospects in computer science. /n 
+		     6. Conversant with computer programming concepts.")
+}
