3b9a18171bba5e21fbdab3e1d43002550743c6b0
gngugnniokl <75842539+gngugnniokl@users.noreply.github.com>
2023-10-22 15:34:35 +0100

    a COmmit

--- week-2/Project-3.rs
+++ week-2/Project-3.rs
@@ -1,16 +1,35 @@
-
 fn main() {
-    let OP = 210000.0;
-    let DR = 5.0;
-    let YRS = 3;
+    // Prices
+    let t = 450000;
+    let h = 750000;
+    let m = 1500000;
+    let d = 2850000;
+    let a = 250000;
 
-    let value_after_3_YRS = calculate_value_after_n_YRS(OP, DR, YRS);
+    // Quantities storage
+    let t_amount = 2;
+    let h_amount = 3;
+    let m_amount = 1;
+    let d_amount = 3;
+    let a_amount = 1;
 
-    println!("The value of the TV after {} years is {:.2} Naira", YRS, value_after_3_YRS);
+    // Amount price product storage
+    let t_sum = t * t_amount;
+    let h_sum = h * h_amount;
+    let m_sum = m * m_amount;
+    let d_sum = d * d_amount;
+    let a_sum = a * a_amount;
+
+    // Sum storage
+    let summation = t_sum + h_sum + m_sum + d_sum + a_sum;
+
+    // Average storage
+    let sum_quantity = t_amount + h_amount + m_amount + d_amount + a_amount;
+    let average = summation as f64 / sum_quantity as f64;
+
+    // outtput
+    println!("Summation: ${}", summation);
+    println!("Average Price: ${:.2}", average);
 }
 
-fn calculate_value_after_n_YRS(OP: f64, DR: f64, YRS: u32) -> f64 {
-    let r = DR / 100.0;
-    let a = OP * (1.0 - r).powi(YRS as i32);
-    a
-}
+
--- week-2/tempCodeRunnerFile.rs
+++ week-2/tempCodeRunnerFile.rs
@@ -1,8 +1,1 @@
-fn main(){
-let P:f64 = 520000000.0;
-let R:f64 = 10.0;
-let T:f64 = 5.0;
-let A:f64 = P*(1+((R/100).powf(T)));
-let CI:f64 = A - P;
-println!("Your compound interest is {} and your amount is {}", CI, A);
-}
+t
