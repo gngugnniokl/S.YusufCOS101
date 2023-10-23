d843f2278fd3c4d45e0a42945fc53cfefdf519ea
gngugnniokl <75842539+gngugnniokl@users.noreply.github.com>
2023-10-23 08:15:36 +0100

    A commitement

--- week-2/Project-3.rs
+++ week-2/Project-3.rs
@@ -1,35 +1,19 @@
 fn main() {
-    // Prices
-    let t = 450000;
-    let h = 750000;
-    let m = 1500000;
-    let d = 2850000;
-    let a = 250000;
+    let initial_value = 210000.0;
+    let annual_depreciation_rate = 0.05; // 5% annual depreciation rate
+    let years = 3;
 
-    // Quantities storage
-    let t_amount = 2;
-    let h_amount = 3;
-    let m_amount = 1;
-    let d_amount = 3;
-    let a_amount = 1;
+    // Calculate the value of the TV after 3 years using the formula
+    let final_value = initial_value * (1.0 - annual_depreciation_rate).powf(years as f64);
 
-    // Amount price product storage
-    let t_sum = t * t_amount;
-    let h_sum = h * h_amount;
-    let m_sum = m * m_amount;
-    let d_sum = d * d_amount;
-    let a_sum = a * a_amount;
-
-    // Sum storage
-    let summation = t_sum + h_sum + m_sum + d_sum + a_sum;
-
-    // Average storage
-    let sum_quantity = t_amount + h_amount + m_amount + d_amount + a_amount;
-    let average = summation as f64 / sum_quantity as f64;
-
-    // outtput
-    println!("Summation: ${}", summation);
-    println!("Average Price: ${:.2}", average);
+    println!("The value of the TV after {} years is: N{:.2}", years, final_value);
 }
+
+
+
+
+
+
+
 
 
