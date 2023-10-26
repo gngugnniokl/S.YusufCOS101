ce03b19d1df90d6335026bdef2541163ced3443c
gngugnniokl <75842539+gngugnniokl@users.noreply.github.com>
2023-10-26 17:14:53 +0100

    comix complex

--- week-3/practice_5/src/main.rs
+++ week-3/practice_5/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let special_character = '@' ;
+    let alphabet:char = 'B';
+    let surnname = "Bolaji";
+    let first_name = "Micheal";
+    let middle_name = "Gboyega";
+
+    println!("");
+    println!("Special character: {}"),special_character);
+    println!("Alphabet: {}", alphabet);
+    println!("Name: {} {} {}",surname,middle_name,firt_name);
 }
---
+++ week-3/practice_9/target/debug/practice_9.d
@@ -0,0 +1,1 @@
+C:\Users\ipinu\S.YusufCOS101\week-3\practice_9\target\debug\practice_9.exe: C:\Users\ipinu\S.YusufCOS101\week-3\practice_9\src\main.rs
